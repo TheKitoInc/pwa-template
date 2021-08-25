d60ddd8796905ff9fad81a28cef11582d655a7fa
TheKito <11139894+TheKito@users.noreply.github.com>
2021-08-24 21:03:58 -0300

    Create serviceWorkerInstaller.js

---
+++ public/serviceWorkerInstaller.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+            .then(function (registration) {
+                registration.addEventListener('updatefound', function () {
+                    var installingWorker = registration.installing;
+                    console.log('A new service worker is being installed:', installingWorker);
+                });
+            })
+            .catch(function (error) {
+                console.log('Service worker registration failed:', error);
+            });
+} else {
+    console.log('Service workers are not supported.');
+}
