97c301f7b98181278ec1280175861041016bee5f
TheKito <11139894+TheKito@users.noreply.github.com>
2022-03-12 19:00:46 -0300

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,73 +1,89 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+const NETWORK_TIMEOUT = 5000;
+const CACHE_NAME = 'v1';
+const CACHE_FILES = ['.'];
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v4';
-const RUNTIME = 'runtime';
+/////////////////////////////////CACHE ACCESS///////////////////////////////////
+// Init cache
+async function initCache() {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        console.log('Service Worker: Init cache: ' + CACHE_NAME);
+        return cache.addAll(CACHE_FILES);
+    });
+}
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-    //'index.php',
-    //'./',
-];
+// Put response in cache
+async function toCache(request, response) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.put(request, response.clone());
+    });
+}
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-    event.waitUntil(
-            caches.open(PRECACHE)
-            .then(cache => cache.addAll(PRECACHE_URLS))
-            .then(self.skipWaiting())
-            );
-});
+// Get response from cache
+async function fromCache(request) {
+    return new Promise(function (fulfill, reject) {
+        caches.open(CACHE_NAME).then(function (cache) {
+            cache.match(request).then(fulfill, reject);
+        }, reject);
+    });
+}
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-            caches.keys().then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-        return Promise.all(cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-        }));
-    }).then(() => self.clients.claim())
-            );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-    // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin))
-    {
-        event.respondWith(
-                caches.match(event.request).then(cachedResponse => {
-            if (cachedResponse)
-            {
-                return cachedResponse;
+// Clean older cache 
+async function cleanCache()
+{
+    caches.keys().then(function (keys) {
+        return keys.map(async function (cache) {
+            if (cache !== CACHE_NAME) {
+                console.log('Service Worker: Removing old cache: ' + cache);
+                return await caches.delete(cache);
             }
+        })
+    })
+}
+/////////////////////////////////NETWORK ACCESS/////////////////////////////////
+async function fromNetwork(request) {
+    return new Promise(function (fulfill, reject) {
+        const timeoutId = setTimeout(reject, NETWORK_TIMEOUT);
+        fetch(request).then(function (response) {
+            clearTimeout(timeoutId);
+            toCache(request, response.clone());
+            fulfill(response);
+        }, reject);
+    });
+}
+/////////////////////////////NETWORK/CACHE ACCESS///////////////////////////////
+async function fromNetworkElseCache(request) {
+    return new Promise(function (fulfill, reject) {
+        fromNetwork(request).then(fulfill, function () {
+            fromCache(request).then(fulfill, reject);
+        });
+    });
+}
+async function fromCacheElseNetwork(request) {
+    return new Promise(function (fulfill, reject) {
+        fromCache(request).then(fulfill, function () {
+            fromNetwork(request).then(fulfill, reject);
+        });
+    });
+}
+////////////////////////////////////////////////////////////////////////////////
+// On install, cache the non available resource.
+self.addEventListener('install', function (evt) {
+    console.log('The service worker is being installed.');
+    cleanCache();
+    evt.waitUntil(initCache().then(function () {
+        return self.skipWaiting();
+    }));
+});
 
-            return caches.open(RUNTIME).then(cache => {
-                return fetch(event.request).then(response => {
-                    // Put a copy of the response in the runtime cache.
-                    return cache.put(event.request, response.clone()).then(() => {
-                        return response;
-                    });
-                });
-            });
-        }));
-    }
+// On activate
+self.addEventListener('activate', function (evt) {
+    console.log('The service worker is being activated.');
+    evt.waitUntil(self.clients.claim());
+});
+
+// On fetch
+self.addEventListener('fetch', function (evt) {
+    //console.log('The service worker is serving the asset: ' + evt.request.url);
+    evt.respondWith(fromNetworkElseCache(evt.request));
+    //evt.respondWith(fromCacheElseNetwork(evt.request));
 });
