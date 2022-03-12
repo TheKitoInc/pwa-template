254199cd907ba5209d8bb307210d24eded80f322
TheKito <11139894+TheKito@users.noreply.github.com>
2022-03-12 19:03:13 -0300

    Update serviceWorkerInstaller.js

--- public/scripts/serviceWorkerInstaller.js
+++ public/scripts/serviceWorkerInstaller.js
@@ -1,20 +1,23 @@
-if ('serviceWorker' in navigator)
+(function ()
 {
-    navigator.serviceWorker.register('/serviceWorker.js')
-            .then(function (registration)
-            {
-                registration.addEventListener('updatefound', function ()
-                {
-                    var installingWorker = registration.installing;
-                    console.log('A new service worker is being installed:', installingWorker);
+    if (('serviceWorker' in navigator))
+    {
+        navigator.serviceWorker.register('serviceWorker.js')
+                .then(function (registration) {
+                    
+                    registration.addEventListener('updatefound', function ()
+                    {
+                        var installingWorker = registration.installing;
+                        console.log('A new service worker is being installed:', installingWorker);
+                        window.setTimeout(function ()
+                        {
+                            location.reload();
+                        }, 1000);
+                    });
+                    
+                })
+                .catch(function (error) {
+                    console.log('Service worker registration failed:', error);
                 });
-            })
-            .catch(function (error)
-            {
-                console.log('Service worker registration failed:', error);
-            });
-}
-else
-{
-    console.log('Service workers are not supported.');
-}
+    }
+}());
