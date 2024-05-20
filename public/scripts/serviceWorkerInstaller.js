50bd520d9e05767b944f194da04c75d1c083cd5e
Kito Git Service <git@thekitoinc.com>
2024-05-20 09:20:10 +0000

    js-standard-js

--- public/scripts/serviceWorkerInstaller.js
+++ public/scripts/serviceWorkerInstaller.js
@@ -1,23 +1,17 @@
-(function ()
-{
-    if (('serviceWorker' in navigator))
-    {
-        navigator.serviceWorker.register('serviceWorker.js')
-                .then(function (registration) {
-                    
-                    registration.addEventListener('updatefound', function ()
-                    {
-                        var installingWorker = registration.installing;
-                        console.log('A new service worker is being installed:', installingWorker);
-                        window.setTimeout(function ()
-                        {
-                            location.reload();
-                        }, 1000);
-                    });
-                    
-                })
-                .catch(function (error) {
-                    console.log('Service worker registration failed:', error);
-                });
-    }
-}());
+(function () {
+  if (('serviceWorker' in navigator)) {
+    navigator.serviceWorker.register('serviceWorker.js')
+      .then(function (registration) {
+        registration.addEventListener('updatefound', function () {
+          const installingWorker = registration.installing
+          console.log('A new service worker is being installed:', installingWorker)
+          window.setTimeout(function () {
+            location.reload()
+          }, 1000)
+        })
+      })
+      .catch(function (error) {
+        console.log('Service worker registration failed:', error)
+      })
+  }
+}())
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,89 +1,88 @@
-const NETWORK_TIMEOUT = 5000;
-const CACHE_NAME = 'v1';
-const CACHE_FILES = ['.'];
+const NETWORK_TIMEOUT = 5000
+const CACHE_NAME = 'v1'
+const CACHE_FILES = ['.']
 
-/////////////////////////////////CACHE ACCESS///////////////////////////////////
+/// //////////////////////////////CACHE ACCESS///////////////////////////////////
 // Init cache
-async function initCache() {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        console.log('Service Worker: Init cache: ' + CACHE_NAME);
-        return cache.addAll(CACHE_FILES);
-    });
+async function initCache () {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    console.log('Service Worker: Init cache: ' + CACHE_NAME)
+    return cache.addAll(CACHE_FILES)
+  })
 }
 
 // Put response in cache
-async function toCache(request, response) {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        return cache.put(request, response.clone());
-    });
+async function toCache (request, response) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.put(request, response.clone())
+  })
 }
 
 // Get response from cache
-async function fromCache(request) {
-    return new Promise(function (fulfill, reject) {
-        caches.open(CACHE_NAME).then(function (cache) {
-            cache.match(request).then(fulfill, reject);
-        }, reject);
-    });
+async function fromCache (request) {
+  return new Promise(function (fulfill, reject) {
+    caches.open(CACHE_NAME).then(function (cache) {
+      cache.match(request).then(fulfill, reject)
+    }, reject)
+  })
 }
 
-// Clean older cache 
-async function cleanCache()
-{
-    caches.keys().then(function (keys) {
-        return keys.map(async function (cache) {
-            if (cache !== CACHE_NAME) {
-                console.log('Service Worker: Removing old cache: ' + cache);
-                return await caches.delete(cache);
-            }
-        })
+// Clean older cache
+async function cleanCache () {
+  caches.keys().then(function (keys) {
+    return keys.map(async function (cache) {
+      if (cache !== CACHE_NAME) {
+        console.log('Service Worker: Removing old cache: ' + cache)
+        return await caches.delete(cache)
+      }
     })
+  })
 }
-/////////////////////////////////NETWORK ACCESS/////////////////////////////////
-async function fromNetwork(request) {
-    return new Promise(function (fulfill, reject) {
-        const timeoutId = setTimeout(reject, NETWORK_TIMEOUT);
-        fetch(request).then(function (response) {
-            clearTimeout(timeoutId);
-            toCache(request, response.clone());
-            fulfill(response);
-        }, reject);
-    });
+/// //////////////////////////////NETWORK ACCESS/////////////////////////////////
+async function fromNetwork (request) {
+  return new Promise(function (fulfill, reject) {
+    const timeoutId = setTimeout(reject, NETWORK_TIMEOUT)
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId)
+      toCache(request, response.clone())
+      fulfill(response)
+    }, reject)
+  })
 }
-/////////////////////////////NETWORK/CACHE ACCESS///////////////////////////////
-async function fromNetworkElseCache(request) {
-    return new Promise(function (fulfill, reject) {
-        fromNetwork(request).then(fulfill, function () {
-            fromCache(request).then(fulfill, reject);
-        });
-    });
+/// //////////////////////////NETWORK/CACHE ACCESS///////////////////////////////
+async function fromNetworkElseCache (request) {
+  return new Promise(function (fulfill, reject) {
+    fromNetwork(request).then(fulfill, function () {
+      fromCache(request).then(fulfill, reject)
+    })
+  })
 }
-async function fromCacheElseNetwork(request) {
-    return new Promise(function (fulfill, reject) {
-        fromCache(request).then(fulfill, function () {
-            fromNetwork(request).then(fulfill, reject);
-        });
-    });
+async function fromCacheElseNetwork (request) {
+  return new Promise(function (fulfill, reject) {
+    fromCache(request).then(fulfill, function () {
+      fromNetwork(request).then(fulfill, reject)
+    })
+  })
 }
-////////////////////////////////////////////////////////////////////////////////
+/// /////////////////////////////////////////////////////////////////////////////
 // On install, cache the non available resource.
 self.addEventListener('install', function (evt) {
-    console.log('The service worker is being installed.');
-    cleanCache();
-    evt.waitUntil(initCache().then(function () {
-        return self.skipWaiting();
-    }));
-});
+  console.log('The service worker is being installed.')
+  cleanCache()
+  evt.waitUntil(initCache().then(function () {
+    return self.skipWaiting()
+  }))
+})
 
 // On activate
 self.addEventListener('activate', function (evt) {
-    console.log('The service worker is being activated.');
-    evt.waitUntil(self.clients.claim());
-});
+  console.log('The service worker is being activated.')
+  evt.waitUntil(self.clients.claim())
+})
 
 // On fetch
 self.addEventListener('fetch', function (evt) {
-    //console.log('The service worker is serving the asset: ' + evt.request.url);
-    evt.respondWith(fromNetworkElseCache(evt.request));
-    //evt.respondWith(fromCacheElseNetwork(evt.request));
-});
+  // console.log('The service worker is serving the asset: ' + evt.request.url);
+  evt.respondWith(fromNetworkElseCache(evt.request))
+  // evt.respondWith(fromCacheElseNetwork(evt.request));
+})
